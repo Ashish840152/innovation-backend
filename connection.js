a1e5272e171655d33bc5f90c214652331716f83c
ashishkumar.yadav <ashishkumar.yadav@fluper.in>
2025-01-27 19:23:15 +0530

    update

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connection = async () => {
   try {
     mongoose.set('strictQuery', true);
-    await mongoose.connect('mongodb://127.0.0.1:27017/machineTest', { useNewUrlParser: true });
+    await mongoose.connect('mongodb://127.0.0.1:27017/InnovationTask', { useNewUrlParser: true });
     console.log('connected');
   } catch (err) {
     console.log(err);
